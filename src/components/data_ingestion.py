c4f293cbd60e03c895a4d438db52c7f8dc93170a
adlirazin7 <adlirazin7@yahoo.com>
2024-12-14 15:35:43 +0800

    Data transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,3 +1,15 @@
+"""
+data_injection.py
+
+This module handles the ingestion of raw data into the project pipeline. It is responsible for:
+- Loading datasets from various sources (e.g., files, databases, APIs).
+- Preparing the train test data
+- Logging the data injection process and managing errors.
+
+Designed to streamline data preparation for downstream processing and analysis.
+"""
+
+
 import os
 import sys
 from src.exception import CustomException
@@ -7,12 +19,16 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 
 @dataclass
 class DataIngestionConfig:
     train_data_path : str=os.path.join('artifacts',"train.csv")
     test_data_path : str=os.path.join('artifacts',"test.csv")
     raw_data_path : str=os.path.join('artifacts',"raw.csv")
+
 
 class DataIngestion:
     def __init__(self):
@@ -41,6 +57,10 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e,sys)
 
+
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data, _ = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
